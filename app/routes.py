6e90b10285488fabc8a0f49a3d150e2ca313e265
cnpapado <14839869+cnpapado@users.noreply.github.com>
2019-09-24 01:32:38 +0300

    search and display results

--- app/routes.py
+++ app/routes.py
@@ -9,4 +9,43 @@ def startpage():
 @app.route('/postme', methods=['POST'])
 def getjsdata():
     artist1 = request.form['artist1']
-    return jsonify({'name':'snarky puppy'})
+    r =   {
+    "resultsPage": {
+      "results": {
+        "artist": [
+          {
+            "id":253846,
+            "uri":"http://www.songkick.com/artists/253846-radiohead",
+            "displayName":"Radiohead",
+            "onTourUntil":"2010-01-01"
+          },
+          {
+            "id":253841,
+            "uri":"http://www.songkick.com/artists/253846-radiohead",
+            "displayName":"The National",
+            "onTourUntil":"2010-01-01"
+          },
+          {
+            "id":253842,
+            "uri":"http://www.songkick.com/artists/253846-radiohead",
+            "displayName":"Arcade Fire",
+            "onTourUntil":"2010-01-01"
+          },
+          {
+            "id":253843,
+            "uri":"http://www.songkick.com/artists/253846-radiohead",
+            "displayName":"Grizzly Bear",
+            "onTourUntil":"2010-01-01"
+          }
+        ]
+      },
+      "totalEntries":4,
+      "perPage":50,
+      "page":1,
+      "status":"ok"
+    }
+  }
+    
+
+
+    return jsonify(r)
--- app/static/search.js
+++ app/static/search.js
@@ -1,19 +1,55 @@
 $(document).ready(function(){
-	$("#artist1button").click(function(){
-        
-	    var artist1=$('#artist1').val();
-        //alert(artist1);
-        $.ajax( "/postme", {
-        	data : {
+	//artist1 = null;
+	$("#artist1button").click(function() { //TODO validate non-emptyness
+		//run only if actually changed
+		//if (artist1 != $('#artist1').val()) {alert(artist1, $('#artist1').val());} //TODO debug here			
+		
+		//empty select
+ 		$('#results').find('option').remove().end();
+		
+        	artist1=$('#artist1').val();
+ 		console.log("POSTTTTTTTT", artist1);
+           	$.ajax( "/postme", {
+        		data : {
 				artist1: artist1
 			},
 			type: 'POST',
-			url: '/postme'    
-        })
-		.done(function(data) { 
-			$('#hiddendiv').text(data.name);
- 		}); 
-    });
+			url: '/postme',
+			success: function(json) {
+				//TODO handle query not found
+				if (json.resultsPage.totalEntries == 1) {
+					$("#artist1").val(data.resultsPage.results.artist[0].displayName);
+					//save artist1=data.resultsPage.results.artist[0].id
+				}
+				else {
+					$("#artist1").val("Many similar results found:");
+					toggle_results(json);			
+				}	
+    			}
+       		})
+		.done(function(data) {
+                	$('#hiddendiv').text(data.resultsPage.results.artist[0].displayName);
+		});
+	});
+
+
+
+	function toggle_results(json) { 
+		$("#results").show();
+	        $.each(json.resultsPage.results.artist, function(i, value) {
+	    		$('#results').append($('<option>').text(value.displayName).attr('value', value.id));	        			
+		});
+		$('#results').attr("size", json.resultsPage.totalEntries);
+		
+		$("#results").on("click", function(event) {
+	        	$("#artist1").val($("#results option:selected").text());
+			$("#results").hide();      	
+		});
+	}
+
+
 });
+
+
 
 
