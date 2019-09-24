35bbec669b4d1d4f2f52708a76526dcb76e9baa6
cnpapado <14839869+cnpapado@users.noreply.github.com>
2019-09-24 15:22:06 +0300

    ui dropdown/buttons fixes

--- app/static/search.js
+++ app/static/search.js
@@ -1,54 +1,71 @@
 $(document).ready(function(){
 	//artist1 = null;
-	$("#artist1button").click(function() { //TODO validate non-emptyness
-		//run only if actually changed
-		//if (artist1 != $('#artist1').val()) {alert(artist1, $('#artist1').val());} //TODO debug here			
-		
-		//empty select
- 		$('#results').find('option').remove().end();
-		
-        	artist1=$('#artist1').val();
- 		console.log("POSTTTTTTTT", artist1);
-           	$.ajax( "/postme", {
-        		data : {
-				artist1: artist1
-			},
-			type: 'POST',
-			url: '/postme',
-			success: function(json) {
-				//TODO handle query not found
-				if (json.resultsPage.totalEntries == 1) {
-					$("#artist1").val(data.resultsPage.results.artist[0].displayName);
-					//save artist1=data.resultsPage.results.artist[0].id
-				}
-				else {
-					$("#artist1").val("Many similar results found:");
-					toggle_results(json);			
-				}	
-    			}
-       		})
-		.done(function(data) {
-                	$('#hiddendiv').text(data.resultsPage.results.artist[0].displayName);
-		});
+	$("#searchmodule > div > input").on('input', function(){
+		let clicked = $(this).parent().parent();
+		$($(this).siblings("button")).html("<i class='fa fa-search'></i>"); 
+		$($(this).siblings("button")).off('click').on('click', function(){
+			main(clicked);
+  		});
 	});
+});	
 
+function main(search_mod) { //TODO validate non-emptyness
+	let thisinput = $(search_mod).children("div").children("input");
+	let thisbutton = $(search_mod).children("div").children("button");
+	let thisselect = $(search_mod).children("select");
 
-
-	function toggle_results(json) { 
-		$("#results").show();
-	        $.each(json.resultsPage.results.artist, function(i, value) {
-	    		$('#results').append($('<option>').text(value.displayName).attr('value', value.id));	        			
-		});
-		$('#results').attr("size", json.resultsPage.totalEntries);
+	//empty select
+	$(thisselect).find('option').remove().end();
 		
-		$("#results").on("click", function(event) {
-	        	$("#artist1").val($("#results option:selected").text());
-			$("#results").hide();      	
-		});
+    artist1=$('#artist1').val();
+	console.log("POSTTTTTTTT", artist1);
+
+	post_to_api().done(function(data) {
+		if (data.resultsPage.totalEntries == 1) {
+			$(thisinput).val(data.resultsPage.results.artist[0].displayName);
+			//save artist1=data.resultsPage.results.artist[0].id
+		}
+		else {
+			$(thisinput).val("Many similar results found:");
+			toggle_results(data, thisselect, thisinput);
+			$(thisbutton).html("<i class='fa fa-caret-down'></i>");
+			$(thisbutton).off('click').on('click', function() { $(thisselect).toggle() });						
+		}
+
+
+
+	});
+	function foo(data, thisselect) {		
+		   			
 	}
+}
 
+function post_to_api() {           	
+	return $.ajax( "/postme", {
+		data : {
+			artist1: artist1
+		},
+		type: 'POST',
+		url: '/postme',
+		success: function(json) {
+			//TODO add failure handlers	
+		}
+     	});
+}
 
-});
+function toggle_results(json, thisselect, thisinput) { 
+	$(thisselect).show();
+        $.each(json.resultsPage.results.artist, function(i, value) {
+    		$(thisselect).append($('<option>').text(value.displayName).attr('value', value.id));	        			
+	});
+	$(thisselect).attr("size", json.resultsPage.totalEntries);
+	
+	$(thisselect).on("click", function(event) {
+       	$(thisinput).val($("#searchmodule > select option:selected").text());
+		$(thisselect).hide();      	
+	});
+}
+
 
 
 
